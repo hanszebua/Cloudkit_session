7e05658dda490d6352ec8d1075d81b032c5729cf
Hans Jonathan Zebua <hans.zebua@icloud.com>
2024-08-18 21:06:49 +0700

    Initial Commit

---
+++ Cloudkit_session/Cloudkit_sessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cloudkit_sessionApp.swift
+//  Cloudkit_session
+//
+//  Created by Hans Zebua on 18/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Cloudkit_sessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cloudkit_session/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cloudkit_session
+//
+//  Created by Hans Zebua on 18/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
